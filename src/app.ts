68cce5f4685d95f2632ee6ab9833dbad7b77c517
Rajmal1 <aeis.gui@hotmail.com>
2022-09-14 01:03:39 -0300

    Index Properties

--- src/app.ts
+++ src/app.ts
@@ -116,3 +116,14 @@ const userInputElement = document.getElementById("user-input");
 if (userInputElement) {
   (userInputElement as HTMLInputElement).value = "Hi there!";
 }
+
+interface ErrorContainer {
+  id: string;
+  [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+  id: "message",
+  email: "Not a valid email!",
+  username: "Must start with a capital character!",
+};
