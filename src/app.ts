79a083c1eecb705cc0851230fb4495c840721900
Rajmal1 <aeis.gui@hotmail.com>
2022-09-13 23:57:22 -0300

    Intersection Types

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,22 @@
-// Code goes here!
+type Admin = {
+  name: string;
+  privilegies: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+let e1: ElevatedEmployee = {
+  name: "Max",
+  privilegies: ["create-server"],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
