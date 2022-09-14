d515d79a45a634217b4bd647aec0ca3b87e0e0bd
Rajmal1 <aeis.gui@hotmail.com>
2022-09-14 00:50:34 -0300

    Discriminated Unions

--- src/app.ts
+++ src/app.ts
@@ -75,3 +75,32 @@ function useVehicle(vehicle: Vehicle) {
 
 useVehicle(v1);
 useVehicle(v2);
+
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+      break;
+  }
+
+  console.log("Mooving at speed " + speed);
+}
+
+moveAnimal({ type: "bird", flyingSpeed: 10 });
+moveAnimal({ type: "horse", runningSpeed: 5 });
