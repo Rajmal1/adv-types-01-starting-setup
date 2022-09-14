50501c7a84d4380f85605e9706cc50364c14e986
Rajmal1 <aeis.gui@hotmail.com>
2022-09-14 01:17:28 -0300

    Optional Chaining

--- src/app.ts
+++ src/app.ts
@@ -35,6 +35,14 @@ function add(a: Combinable, b: Combinable) {
 
 const result = add(1, "Max");
 
+const fetchedUserData = {
+  id: "u1",
+  name: "Max",
+  job: { title: "CEO", description: "My own company" },
+};
+
+console.log(fetchedUserData?.job?.title);
+
 // type UnknownEmployee = Employee | Admin;
 
 // function printEmployeeInfo(emp: UnknownEmployee) {
